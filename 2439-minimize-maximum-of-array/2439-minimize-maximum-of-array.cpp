8b564d4f886a118737d4c1d02a624a801ea2d441
UTKARSH SAH <utkarshsah2001@gmail.com>
2023-04-05 19:13:44 +0530

    Time: 128 ms (89.13%), Space: 71.4 MB (61.54%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int minimizeArrayValue(vector<int>& nums) 
+    {
+        int n=(int)nums.size();
+        long long sum=0,ans=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            long long res=(sum+i)/(i+1);
+            ans=max(ans,res);
+        }
+        return ans;
+    }
+};
