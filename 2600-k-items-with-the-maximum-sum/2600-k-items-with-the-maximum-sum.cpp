3ffe51294fcad21d742a195bf8521facfb564836
UTKARSH SAH <utkarshsah2001@gmail.com>
2023-04-26 22:43:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.90%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(numOnes>=k)
+        {
+            return k;
+        }
+        else if((numOnes+numZeros)>=k)
+        {
+            return numOnes;
+        }
+        else
+        {
+            return 2*numOnes+numZeros-k;
+        }
+    }
+};
